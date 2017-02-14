31fe1acb3171c3a9f18d65691e6545175ead799a
Anthony Ngene <ngeneanthony@gmail.com>
2017-02-14 15:12:27 +0100

    add getIp which retrieves user IP address using a public api

--- index.js
+++ index.js
@@ -13,3 +13,71 @@ console.log(
     figlet.textSync('Welcome!', { horizontalLayout: 'full' })
   )
 );
+
+//Function to get current ip address
+function getIp () {
+    var status = new Spinner('Getting your IP, please wait...');
+    status.start();
+
+    //Request the IP address
+    request('https://api.ipify.org?format=json', function (err, res, data){
+         if (err) {
+			console.log(err)
+			return;
+		}
+        data = JSON.parse(data);
+        status.stop();
+        console.log('\nYour IP is: ' + data.ip + '\n');
+        exitApp();
+    });
+}
+
+
+//Function that starts the app
+function startApp() {
+    inquirer.prompt([ {
+    type: 'list',
+    name: 'option',
+    message: 'What do you want to do?',
+    choices: [
+      'Check my Ip address',
+      'Search for a book (e.g: Things fall apart)',
+      'Search for a location (e.g: Lagos University)',
+    ]
+  } ]).then(function (answer) {
+      
+        if(answer.option === 'Check my Ip address'){
+            getIp();
+        };
+
+        if(answer.option === 'Search for a book (e.g: Things fall apart)'){
+            findBook();
+        }
+
+        if(answer.option === 'Search for a location (e.g: Lagos University)'){
+            getLocation();
+        }
+
+    });
+}
+
+//Function that Exits the app
+function exitApp() {
+    inquirer.prompt([ {
+    type: 'list',
+    name: 'option',
+    message: 'Exit?',
+    choices: [
+      'Yes',
+      'No',
+    ]
+  } ]).then(function (answer) {
+      
+        if(answer.option === 'No') return startApp();
+
+        if(answer.option === 'Yes') return console.log(chalk.yellow('=============Thanks for using this app!============='));
+
+    });
+}
+
+startApp();
