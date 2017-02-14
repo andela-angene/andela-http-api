75bad0d8464b2dd32d48720badfddeef850b0f53
Anthony Ngene <ngeneanthony@gmail.com>
2017-02-14 15:10:12 +0100

    create index.js and write welcome message

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var request = require('request'),
+    clear   = require('clear'),
+    figlet  = require('figlet'),
+    inquirer    = require('inquirer'),
+    chalk       = require('chalk'),
+    Spinner     = require('clui').Spinner;
+
+clear();
+console.log(
+  chalk.green(
+    figlet.textSync('Welcome!', { horizontalLayout: 'full' })
+  )
+);
